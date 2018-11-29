68032717aabdc468c95130e01c01180a9bb398c8
shashank.s <shashank.s@gramener.com>
2018-11-29 12:21:23 +0530

    initial commit

---
+++ bitcoin_price/bitcoin.py
@@ -0,0 +1,58 @@
+import requests
+from datetime import datetime
+import time
+
+bitcoin_api_url = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
+ifttt_webhook_url = 'https://maker.ifttt.com/trigger/{}/with/key/{}'
+BITCOIN_PRICE_THRESHOLD = 8000
+
+
+
+def get_latest_bitcoin_price():
+    response = requests.get(bitcoin_api_url)
+    response_json = response.json()
+    return float(response_json[0]['price_usd'])
+
+
+
+def post_ifttt_webhook(value):
+    data = {'value1': value}
+    # ifttt_event_url = IFTTT_WEBHOOKS_URL.format(event)
+    requests.post(ifttt_webhook_url, json=data)
+
+
+
+def format_bitcoin_history(bitcoin_history):
+    rows = []
+    for bitcoin_price in bitcoin_history:
+        date = bitcoin_price['date'].strftime('%d.%m.%Y %H:%M')
+        price = bitcoin_price['price']
+        row = '{}: $<b>{}</b>'.format(date, price)
+        rows.append(row)
+
+    return '<br>'.join(rows)
+
+
+
+def main():
+    bitcoin_history = []
+    while True:
+        price = get_latest_bitcoin_price()
+        date = datetime.now()
+        bitcoin_history.append({'date': date, 'price': price})
+
+        # Send an emergency notification
+        if price < BITCOIN_PRICE_THRESHOLD:
+            post_ifttt_webhook(price)
+
+        if len(bitcoin_history) == 5:
+            post_ifttt_webhook(format_bitcoin_history(bitcoin_history))
+            # Reset the history
+            bitcoin_history = []
+
+        time.sleep(5 * 60)
+
+
+
+if __name__ == '__main__':
+    main()
